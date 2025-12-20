091eee64bf28656b6510374a0b9327e6b78fdc73
Quidyz <95502942+QuidyzF@users.noreply.github.com>
2025-12-20 20:54:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def debugger(func):
     def wrapper(*args, **kwargs):
         class_name = args[0].__class__.__name__
         print(f'DEBUG: {class_name}({func.__name__})')
-        print(f'args: {args}')
+        print(f'    --> args: {args}')
         return func(*args, **kwargs)
     return wrapper
 
@@ -16,9 +16,9 @@ _id_counter = itertools.count(1)
 @dataclass
 class Order:
     """Класс Заявок, хранит детали"""
-    stock: str # Action name
-    order_type: str # BUY | SELL
-    order_kind: str # LMT | MKT
+    stock: str # stock name
+    action: str # BUY | SELL
+    action_type: str # LMT | MKT
     quantity: int # Count of action
     price: float | None = None # Price of action
 
@@ -32,10 +32,10 @@ class Order:
         if self.quantity <= 0:
             raise ValueError("Количество должно быть положительным.")
 
-        if self.order_kind == 'LMT' and (self.price is None or self.price <= 0):
+        if self.action_type == 'LMT' and (self.price is None or self.price <= 0):
             raise ValueError("Для лимитной заявки цена должна быть указана.")
 
-        if self.order_kind == 'MKT' and self.price is not None:
+        if self.action_type == 'MKT' and self.price is not None:
             raise ValueError("Для рыночной заявки цена не нужна.")
 
     def get_quantity(self) -> int:
@@ -89,13 +89,62 @@ class OrderBook:
         for i, counter_order in enumerate(counter_orders):
             if quantity_to_close_order <= 0:
                 break
+        pass
 
+    def process_order(self, new_order):
 
-
+        if new_order.action_type == 'LMT':
+            pass
+        else:
+            pass
 class Exchange:
     """Главное управление всей биржей. Команды пользователя и хранит историю сделок/статусы заявок"""
+    def __init__(self):
+        self.books = {}
+        self.orders_list = {}
 
-    pass
+    def _get_or_create_order(self, stock):
+        if stock not in self.books:
+            self.books[stock] = OrderBook(stock)
+        return self.books[stock]
+
+    @debugger
+    def place_order(self, action, stock, action_type, price, quantity):
+
+        try:
+            new_order = Order(
+                stock=stock,
+                action = action,
+                action_type = action_type,
+                price = price,
+                quantity = quantity
+            )
+        except ValueError as e:
+            print(f'Ошибка в Exchange.place_order() в try: {e}')
+
+        self.orders_list[new_order.id] = new_order
+        book = self._get_or_create_order(stock)
+
+        # Здесь вставить обработку операции через class Book
+        book.process_order(new_order)
+        # --- Возвращение необходимого print ---
+        if action_type == 'LMT':
+            action_info = 'лимитную заявку'
+        action_info = 'рыночную заявку'
+
+        return f"Вы разместили {action_info} на покупку {quantity} акций {stock} по цене ${price} за штуку."
+
+    def view_orders(self):
+        output = []
+
+        sorted_order_list = sorted(self.orders_list.values(), key=lambda x: x.id)
+
+        for i, order in enumerate(sorted_order_list, 1):
+            line = f'{i}. {order.stock} {order.action_type} {order.action} {order.price} {order.quantity} {order.filled_quantity}/{order.quantity} {order.status}'
+
+            output.append(line)
+
+        return '\n'.join(output)
 
 class Parser:
     """Парсинг входных данных и передача в главный орган"""
@@ -147,15 +196,16 @@ class Parser:
             action = action
             stock, action_type, price, quantity = self._command_parser(parts)
 
-            # Здесь мы относимся к классу Exchange и выдает информацию.
+            # Здесь мы относимся к классу Exchange и выдаём информацию.
             # Action: BUY FB MKT 20
             # Вы разместили рыночную заявку на покупку 20 акций FB.
-            result = None
+            result = self.core.place_order(action, stock, action_type, price, quantity)
+            print(result)
 
         except ValueError as e:
-            print(f'Ошибка {e}')
+            print(f'Сработка ValueError. Ошибка {e}')
         except TypeError as e:
-            print(f'Ошибка {e}')
+            print(f'Сработка TypeError. Ошибка {e}')
 
     @debugger
     def quote_command(self, action, parts):
@@ -164,8 +214,11 @@ class Parser:
 
     @debugger
     def view_command(self, action, parts):
-        print(self.OrderBook.get_quote())
-
+        if len(parts) == 1 and action == 'VIEW' and parts[0] == 'ORDERS':
+            print(self.core.view_orders())
+            print(self.core.books, self.core.orders_list)
+        else:
+            raise ValueError("Может вы имели ввиду VIEW ORDERS ?")
 
     def run(self):
         while True:
